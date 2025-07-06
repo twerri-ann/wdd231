0e7e567691c7ae7ef69423409cbc6716788b1011
twerri-ann <terriannbassaragh491@gmail.com>
2025-07-05 19:27:34 -0500

    added

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,10 @@
 const courses = [
-  { code: "WDD 130", name: "Web Fundamentals", credits: 3, completed: true },
-  { code: "WDD 131", name: "Dynamic Web Fundamentals", credits: 3, completed: true },
-  { code: "WDD 230", name: "Web Frontend Development I", credits: 3, completed: false },
+  { code: "WDD 130", name: "Web Fundamentals", credits: 2, completed: true },
+  { code: "WDD 131", name: "Dynamic Web Fundamentals", credits: 2, completed: true },
+  { code: "WDD 230", name: "Web Frontend Development I", credits: 2, completed: false },
   { code: "CSE 110", name: "Intro to Programming", credits: 2, completed: true },
-  { code: "CSE 111", name: "Programming with Functions", credits: 2, completed: false }
+  { code: "CSE 111", name: "Programming with Functions", credits: 2, completed: true},
+  { code: "CSE 210", name: "Programming with Classes", credits: 2, completed: true}
 ];
 
 const coursesDiv = document.getElementById('courses');
