afb0bb242a6c64e721927192bf5fc54566f1da37
twerri-ann <terriannbassaragh491@gmail.com>
2025-08-01 20:08:15 -0500

    added, adjusted and sorted

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,7 @@
+const yearSpan = document.getElementById("year");
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+
+const modifiedSpan = document.getElementById("modified");
+modifiedSpan.textContent = document.lastModified;
